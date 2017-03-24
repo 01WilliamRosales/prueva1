3a0208743d77485a0dbd143c32034c232bddabf7
PC_ASUS <PC_ASUS@192.168.1.8>
2017-03-23 20:42:26 -0600

    este es una modificacion 1

---
+++ src/gruposis11a/Gruposis11a.java
@@ -0,0 +1,17 @@
+package gruposis11a;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author WILLIAM ROSALES
+ */
+public class Gruposis11a {
+
+    
+    public static void main(String[] args) {
+    String v1="Hola";
+        System.out.println("mensaje:"+v1);
+    }
+    
+}
